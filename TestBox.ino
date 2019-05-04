2ba9edf127ab31e5b6ea7a57637eeb6287733276
Robert <robertgz@users.noreply.github.com>
2019-05-04 15:01:33 -0700

    Removed code for flow sensor from main .ino file

--- TestBox.ino
+++ TestBox.ino
@@ -15,21 +15,12 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 unsigned long previousUpdateTime;
 const unsigned long updateRate = 1000; // in milliseconds
 
-
 const int flowSensorPin = 2;
-volatile unsigned long pulseCount;
-// unsigned long flowMeasurementElapsedTime; // time since last measured flow
-unsigned long flowLastMeasuredTime; // time of last measured flow
-// int output_flow = 0;
-float output_flow = 0;
-
-// new
 FlowSensor flowSensor(flowSensorPin);
 
 const int pressureSensorPin = A0;
 int output_pressure = 0;
 int pressure_sensor_reading = 0;
-
 
 int output_temperature = 0;
 
@@ -39,24 +30,18 @@ void setup() {
 
   lcd.begin(20,4);               // initialize the lcd  
 
-  // // setup for the flow sensor
-  // pulseCount = 0;
-  // pinMode(flowSensorPin, INPUT);
-  // flowLastMeasuredTime = millis();  
-  // attachInterrupt(digitalPinToInterrupt(flowSensorPin), pulseCounter, FALLING);
-  // // attachInterrupt(digitalPinToInterrupt(flowSensorPin), pulseCounter, RISING);
-
   previousUpdateTime = millis();
 }
 
 void loop() {
   unsigned long elapsedTime = millis() - previousUpdateTime;
-  elapsedTime = abs(elapsedTime);  
+  elapsedTime = abs(elapsedTime);
+
+  float output_flow = 0.0f;
 
   // update every updateRate milliseconds
   if (elapsedTime >= updateRate) {
 
-    // output_flow = getFlowRate();
     output_flow = flowSensor.getFlowRate(); // new
 
     output_pressure = getPressure();
@@ -86,47 +71,17 @@ int getPressure(void) {
   return pressure_PSI;    
 }
 
-void pulseCounter() {
-  pulseCount++;
-}
-
-int getFlowRate(void) { // should this return a float?
-  unsigned long flowMeasurementElapsedTime;
-  unsigned long pulses;
-
-  noInterrupts();
-  pulses = pulseCount;
-  pulseCount = 0;
-  flowMeasurementElapsedTime = millis() - flowLastMeasuredTime;
-  flowLastMeasuredTime = millis(); 
-  interrupts();
-
-  // Pulse constant:  0.46mL/Pulse = 0.00046L/Pulse
-  // compute flow rate to gallons/minute
-  float Liters = 0.00046 * pulses;
-  float LitersPerMs = Liters / flowMeasurementElapsedTime;
-  float LitersPerMinute = Liters / (flowMeasurementElapsedTime / 60000.0); 
-  float GalllonsPerMinute = (Liters / 3.785412) / (flowMeasurementElapsedTime / 60000.0); 
-  
-  return GalllonsPerMinute;
-}
-
 void updateLCD(float flow, int pressure, int temperature) {
     lcd.setCursor( 0, 0 );            // go to the first row
     lcd.print("Test Box Sensor Data");
 
     // output Flow rate
-        // needs testing
-    char flowOutput[41]; // needs testing
-    flowSensor.getFormatedOutput(flowOutput, flow); // new
-    // lcd.setCursor( 0, 1 );            // go to the second row
-    // lcd.print(flowOutput);
-     // needs testing
-
+    // needs testing
+    char flowOutput[41];
+    flowSensor.getFormatedOutput(flowOutput, flow);
     lcd.setCursor( 0, 1 );            // go to the second row
-    lcd.print("Flow: ");
-    lcd.print(flow); // change to strFlow
-    lcd.print(" gal/min    ");
+    lcd.print(flowOutput);
+     // needs testing
 
     // output Pressure
     lcd.setCursor( 0, 2 );            // go to the third row
